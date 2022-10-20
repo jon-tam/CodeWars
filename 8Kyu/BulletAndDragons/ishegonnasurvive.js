1ae8b07f00a4ca19ca17378882d526b85a3650a4
jon-tam <jtam168@gmail.com>
2022-10-20 13:08:43 -0700

    Coded solution in one line with fat arrow function

---
+++ 8Kyu/BulletAndDragons/ishegonnasurvive.js
@@ -0,0 +1,8 @@
+//My Solution - One line fat arrow function
+const hero = (bullets, dragons) => (bullets / 2) >= dragons ? true : false;
+
+
+//Best Practice - Straight forward
+// function hero(bullets, dragons){
+//     return bullets >= dragons * 2
+//   }
