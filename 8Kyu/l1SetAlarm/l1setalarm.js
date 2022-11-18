0a39f6f7aab142dd0450936db8690c2d1a172956
jon-tam <jtam168@gmail.com>
2022-11-18 15:41:25 -0800

    Solved with Ternary Operator

---
+++ 8Kyu/l1SetAlarm/l1setalarm.js
@@ -0,0 +1,7 @@
+//My solution
+function setAlarm(employed, vacation){
+    return employed && !vacation ? true : false
+  }
+
+//Solved with fat arrow function
+//const setAlarm = (employed, vacation) => employed && !vacation;
