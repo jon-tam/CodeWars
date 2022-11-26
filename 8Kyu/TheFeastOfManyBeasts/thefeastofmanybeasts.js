624df52ddc0e6d49736f9d394f39afbfcaf0c78a
jon-tam <jtam168@gmail.com>
2022-11-25 20:43:15 -0800

    Solved with ternary operator

---
+++ 8Kyu/TheFeastOfManyBeasts/thefeastofmanybeasts.js
@@ -0,0 +1,6 @@
+//My Solution
+function feast(beast, dish) {
+ return beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1] ? true : false
+}
+
+//Ternary not needed, implicit return of true or false
