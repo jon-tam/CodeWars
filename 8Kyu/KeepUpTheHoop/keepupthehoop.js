47e841357f08ac45639772fd889b8a9528d3d6c0
jon-tam <jtam168@gmail.com>
2022-11-29 10:14:56 -0800

    Solved with ternary operator

---
+++ 8Kyu/KeepUpTheHoop/keepupthehoop.js
@@ -0,0 +1,8 @@
+//P n = how many times the hoop goes round
+//R return two different strings depending on true or false
+//E n = 10, "great, now move on to tricks" n = 5, "Keep at it until you get it"
+//P if n >= 10, then return "great, now move on to tricks". If n < 10, then return "keep at it until you get it"
+
+function hoopCount (n) {
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"    
+ }
