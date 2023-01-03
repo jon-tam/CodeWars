d780704ab52e5cd4899c57db1d2b1df7b65e2d48
jon-tam <jtam168@gmail.com>
2023-01-02 18:32:27 -0800

    Solved with for loop and conditionals pushed into an array

---
+++ 7Kyu/FizzBuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+// Return an array
+function fizzbuzz(n)
+{
+  let result = []
+  
+  for(let i = 1; i <= n; i++){
+    if(i%3 === 0 && i%5 === 0){
+      result.push('FizzBuzz')
+    }else if(i%3 === 0){
+      result.push('Fizz')
+    }else if(i%5 === 0){
+      result.push('Buzz')
+    }else{
+      result.push(i)
+    }
+  }
+  
+  return result
+}
